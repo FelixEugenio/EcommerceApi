06da0bac9d5d85f120db8343ee7e0772296807e8
felix mavila <felixmacarenco@gmail.com>
2024-05-19 04:54:03 +0100

    feat: create getAllProductController feature

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { createProductController, getProductController } = require('../controllers/ProductController');
+
+const { 
+    createProductController,
+     getProductController,
+     getAllProductController
+     } = require('../controllers/ProductController');
+
 const {authMiddleware,isAdmin} = require('../middlewares/auth/authMiddleware');
 
 const router = express.Router();
 
-
 router.post('/product',createProductController);
 router.get('/product/:id',getProductController);
+router.get('/product',getAllProductController);
 module.exports = router;
